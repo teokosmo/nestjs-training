0e82062c9684854634d47d842b04266e7b1cb855
Theodoros Kosmopoulos <Theodoros.Kosmopoulos@arhs-dev-hellas.com>
2023-01-12 00:15:35 +0200

    set events controller methods

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,15 +1,33 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { identity } from 'rxjs';
 
 @Controller('/events')
 export class EventsController {
   @Get()
-  findAll() {}
-  @Get()
-  findOne() {}
+  findAll() {
+    return [
+      { id: 1, name: 'first event' },
+      { id: 2, name: 'second event' },
+    ];
+  }
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return id;
+  }
   @Post()
-  create() {}
-  @Patch()
-  update() {}
-  @Delete()
-  remove() {}
+  create(@Body() input) {
+    return input;
+  }
+  @Patch(':id')
+  update(@Param('id') id, @Body() input) {}
+  @Delete(':id')
+  remove(@Param('id') id) {}
 }
