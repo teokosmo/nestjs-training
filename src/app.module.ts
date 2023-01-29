062177da21056da9650fc9354281a074919be4c3
Theodoros Kosmopoulos <Theodoros.Kosmopoulos@arhs-dev-hellas.com>
2023-01-29 18:29:07 +0200

    create events module
    nest cli command:
    nest generate module events

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EventsController } from './events.controller';
-import { Event } from './event.entity';
+import { Event } from './events/event.entity';
+import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
@@ -17,9 +17,9 @@ import { Event } from './event.entity';
       entities: [Event],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([Event])
+    EventsModule
   ],
-  controllers: [AppController, EventsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
