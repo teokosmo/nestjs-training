fb9ae4123328945153d6abe87db06dfc8b6b4aba
Theodoros Kosmopoulos <Theodoros.Kosmopoulos@arhs-dev-hellas.com>
2023-02-05 13:53:31 +0200

    one-to-many relation

--- src/events/event.entity.ts
+++ src/events/event.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Attendee } from './attendee.entity';
 
 @Entity('events')
 export class Event {
@@ -16,4 +17,7 @@ export class Event {
 
   @Column()
   when: Date;
+
+  @OneToMany(() => Attendee, (attendee) => attendee.event)
+  attendees: Attendee[];
 }
