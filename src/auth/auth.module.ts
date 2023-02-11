1c8b5c8662993ec3c1e56864fa5f685b3f358b75
Theodoros Kosmopoulos <Theodoros.Kosmopoulos@arhs-dev-hellas.com>
2023-02-11 14:22:40 +0200

    generate JWT token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { User } from './user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.AUTH_SECRET,
+        signOptions: {
+          expiresIn: '60m',
+        },
+      })
+    }),
+  ],
   controllers: [AuthController],
-  providers: [LocalStrategy],
+  providers: [LocalStrategy, AuthService],
 })
 export class AuthModule {}
