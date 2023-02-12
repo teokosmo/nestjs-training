ea27e429f95d971d9fe420d8b7a885651cabf189
Theodoros Kosmopoulos <Theodoros.Kosmopoulos@arhs-dev-hellas.com>
2023-02-12 10:01:20 +0200

    create custom decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,23 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './current-user.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService){}
   @Post('login')
   @UseGuards(AuthGuard('local'))
-  async login(@Request() request) {
+  async login(@CurrentUser() user) {
     return {
-      userId: request.user.id,
-      token: this.authService.getTokenForUser(request.user),
+      userId: user.id,
+      token: this.authService.getTokenForUser(user),
     };
   }
 
   @Get('profile')
   @UseGuards(AuthGuard('jwt'))
-  async getProfile(@Request() request) {
-    return request.user;
+  async getProfile(@CurrentUser() user) {
+    return user;
   }
 }
