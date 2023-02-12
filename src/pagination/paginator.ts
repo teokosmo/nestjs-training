276be1ebfa7a56702d41dbdbdaadb4074659f1f3
Theodoros Kosmopoulos <Theodoros.Kosmopoulos@arhs-dev-hellas.com>
2023-02-12 15:07:47 +0200

    serialize deeply nested objects

--- src/pagination/paginator.ts
+++ src/pagination/paginator.ts
@@ -1,3 +1,4 @@
+import { Expose } from 'class-transformer';
 import { SelectQueryBuilder } from 'typeorm';
 
 export interface PaginateOptions {
@@ -6,11 +7,20 @@ export interface PaginateOptions {
   total?: boolean;
 }
 
-export interface PaginationResult<T> {
+export class PaginationResult<T> {
+  constructor(partial: Partial<PaginationResult<T>>) {
+    Object.assign(this, partial);
+  }
+
+  @Expose()
   first: number;
+  @Expose()
   last: number;
+  @Expose()
   limit: number;
+  @Expose()
   total?: number;
+  @Expose()
   data: T[];
 }
 
@@ -24,11 +34,19 @@ export async function paginate<T>(
   const offset = (options.currentPage - 1) * options.limit;
   const data = await qb.limit(options.limit).offset(offset).getMany();
 
-  return {
+  // return {
+  //   first: offset + 1,
+  //   last: offset + data.length,
+  //   limit: options.limit,
+  //   total: options.total ? await qb.getCount() : null,
+  //   data,
+  // };
+
+  return new PaginationResult({
     first: offset + 1,
     last: offset + data.length,
     limit: options.limit,
     total: options.total ? await qb.getCount() : null,
-    data,
-  };
+    data
+  });
 }
